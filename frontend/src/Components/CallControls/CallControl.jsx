7e4638ebd2095fa1225dfd134a6d40015a9e6d4d
deepak <deepakdpyqaz@gmail.com>
2021-06-22 18:03:39 +0530

    Adding linting to frontend

--- frontend/src/Components/CallControls/CallControl.jsx
+++ frontend/src/Components/CallControls/CallControl.jsx
@@ -1,33 +1,37 @@
-// This will contain the controls of the calls 
-import React, { useState } from 'react';
-import Button from '@material-ui/core/Button';
-import CallEndIcon from '@material-ui/icons/CallEnd';
-import MicOffIcon from '@material-ui/icons/MicOff';
-import MicIcon from '@material-ui/icons/Mic';
-import VideocamIcon from '@material-ui/icons/Videocam';
-import VideocamOffIcon from '@material-ui/icons/VideocamOff';
-import './callcontrol.scss';
+// This will contain the controls of the calls
+import React, {useState} from "react";
+import Button from "@material-ui/core/Button";
+import CallEndIcon from "@material-ui/icons/CallEnd";
+import MicOffIcon from "@material-ui/icons/MicOff";
+import MicIcon from "@material-ui/icons/Mic";
+import VideocamIcon from "@material-ui/icons/Videocam";
+import VideocamOffIcon from "@material-ui/icons/VideocamOff";
+import "./callcontrol.scss";
+import PropTypes from "prop-types";
 const CallControl = (props) => {
-    const [control, setControl] = useState({ 'microphone': true, 'camera': true });
-    const switchControl = (controlProperty) => {
-        setControl((prevState) => {
-            let newState = {...prevState,[controlProperty]:!prevState[controlProperty]};
-            props.changeControl(newState);
-            return newState;
-        })
-    }
+  const [control, setControl] = useState({"microphone": true, "camera": true});
+  const switchControl = (controlProperty) => {
+    setControl((prevState) => {
+      const newState = {...prevState, [controlProperty]: !prevState[controlProperty]};
+      props.changeControl(newState);
+      return newState;
+    });
+  };
 
-    return (
-        <div className='call_control'>
-            <Button variant="contained" color="secondary" onClick={() => switchControl("microphone")}>
-                {(control.microphone ? <MicOffIcon /> : <MicIcon />)}
-            </Button>
-            <Button variant="contained" color='secondary'><CallEndIcon /></Button>
-            <Button variant="contained" color="secondary" onClick={() => switchControl("camera")}>
-                {(control.camera ? <VideocamOffIcon /> : <VideocamIcon />)}
-            </Button>
-        </div>
-    )
-}
+  return (
+    <div className='call_control'>
+      <Button variant="contained" color="secondary" onClick={() => switchControl("microphone")}>
+        {(control.microphone ? <MicOffIcon /> : <MicIcon />)}
+      </Button>
+      <Button variant="contained" color='secondary'><CallEndIcon /></Button>
+      <Button variant="contained" color="secondary" onClick={() => switchControl("camera")}>
+        {(control.camera ? <VideocamOffIcon /> : <VideocamIcon />)}
+      </Button>
+    </div>
+  );
+};
 
+CallControl.propTypes={
+  changeControl: PropTypes.func.isRequired,
+};
 export default CallControl;
--- frontend/src/Components/VideoDisplay/VideoDisplay.jsx
+++ frontend/src/Components/VideoDisplay/VideoDisplay.jsx
@@ -1,71 +1,53 @@
 // This component is for displaying video
-import React from 'react';
-import './videodisplay.scss';
-import captureSelfVideo from '../SelfVideo/SelfVideo';
-import video from '../../assets/video.mp4';
-// const VideoDisplay = (props)=>{
-//     const [source,setSource] =  useState(null);
-//     const classList = ['video_wrapper'];
-//     if(props.stream){
-//     console.log(props.stream.getTracks());
-//         setSource()
-//     }
-//     if(props.self)
-//     {
-//         classList.push('video_wrapper--self');
-//     }
-//     return(
-//         <>
-
-//             <div className={classList.join(" ")}>
-//                 <video src={video} controls></video>
-//             </div>
-//         </>
-//     )
-// }
-// export default VideoDisplay;
-
+import React from "react";
+import "./videodisplay.scss";
+import captureSelfVideo from "../SelfVideo/SelfVideo";
+import video from "../../assets/video.mp4";
+import PropTypes from "prop-types";
 class VideoDisplay extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { classList: ['video_wrapper'] };
-        this.videoRef = React.createRef();
+  constructor(props) {
+    super(props);
+    this.state = {classList: ["video_wrapper"]};
+    this.videoRef = React.createRef();
+  }
+  componentDidMount() {
+    if (this.props.self) {
+      this.setState((prevState, props) => {
+        const newClassList = [...this.state.classList, "video_wrapper--self"];
+        return {
+          ...prevState,
+          classList: newClassList,
+        };
+      });
+
+      captureSelfVideo(this.props.selfStreamControl).then((stream) => {
+        this.videoRef.current.srcObject = stream;
+      });
     }
-    componentDidMount() {
-        if (this.props.self) {
-            this.setState((prevState, props) => {
-                let newClassList = [...this.state.classList, 'video_wrapper--self'];
-                return {
-                    ...prevState,
-                    classList: newClassList
-                }
-            })
-            
-            captureSelfVideo(this.props.selfStreamControl).then((stream) => {
-                this.videoRef.current.srcObject = stream;
-            })
-        }
+  }
+  componentDidUpdate() {
+    if (this.props.selfStreamControl) {
+      if (this.props.selfStreamControl.audio || this.props.selfStreamControl.video) {
+        captureSelfVideo(this.props.selfStreamControl).then((stream) => {
+          this.videoRef.current.srcObject = stream;
+        });
+      } else {
+        this.videoRef.current.srcObject = null;
+      }
     }
-    componentDidUpdate() {
-        if (this.props.selfStreamControl) {
-            if (this.props.selfStreamControl.audio || this.props.selfStreamControl.video) {
-                captureSelfVideo(this.props.selfStreamControl).then((stream) => {
-                    this.videoRef.current.srcObject = stream;
-                })
-            }
-            else {
-                this.videoRef.current.srcObject = null;
-            }
-        }
-    }
-    render() {
-        return (<>
-            <div className={this.state.classList.join(" ")}>
-                <video ref={this.videoRef} autoPlay muted></video>
-            </div>
-        </>
-        )
-    }
+  }
+  render() {
+    return (<>
+      <div className={this.state.classList.join(" ")}>
+        <video ref={this.videoRef} autoPlay muted></video>
+      </div>
+    </>
+    );
+  }
 }
 
+VideoDisplay.propTypes={
+  self: PropTypes.bool,
+  selfStreamControl: PropTypes.object,
+};
 export default VideoDisplay;
