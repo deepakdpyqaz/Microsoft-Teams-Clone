1dfc8b789f957022c584aff24fd9fb972dc24996
deepak <deepakdpyqaz@gmail.com>
2021-06-22 17:08:42 +0530

    added feature of camera and microphone to self video

--- frontend/src/Views/VideoCallScreen/VideoCallScreen.jsx
+++ frontend/src/Views/VideoCallScreen/VideoCallScreen.jsx
@@ -3,17 +3,17 @@ import CallControl from "../../Components/CallControls/CallControl";
 import VideoDisplay from "../../Components/VideoDisplay/VideoDisplay";
 import './videocallscreen.scss';
 const VideoCallScreen = () => {
-    const [selfStream,setSelfStream] = useState(null);
+    const [selfStreamControl,setSelfStreamControl] = useState({'audio':true,'video':true});
 
     
     const handleChangeControl=(controls) => {
-        console.log(controls);
+        setSelfStreamControl({'audio':controls.microphone,'video':controls.camera})
     }
     return (
         <>
             <div className="video_display_wrapper">
                 <VideoDisplay self={false} />
-                <VideoDisplay stream={selfStream} self={true} />
+                <VideoDisplay selfStreamControl={selfStreamControl} self={true} />
             </div>
             <CallControl changeControl={handleChangeControl}/>
         </>
